8664def79529d1c3432db9642fc37446d56ecf53
theAdhocracy <murrayadcock@gmail.com>
2021-05-19 02:09:06 +0100

    :sparkles: Footer: New component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledFooter = styled.footer`
+	display: flex;
+	place-content: center;
+	place-items: center;
+	min-height: 5rem;
+	background-color: #2c2727;
+	color: #ffffff;
+	text-align: center;
+`
+
+export const Footer = () => {
+	return (
+		<StyledFooter>
+			<p>Powered by 🎼</p>
+		</StyledFooter>
+	)
+}
