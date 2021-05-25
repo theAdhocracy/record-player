53103b0ab22dc916314aa5e9d82fc4ddc8c670dd
theAdhocracy <murrayadcock@gmail.com>
2021-05-26 00:12:52 +0100

    :wrench: Babel: Styled components are now server rendered

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,49 @@
-import "../styles/globals.css";
-import { AppProps } from "next/app";
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  	html,
+	body {
+		padding: 0;
+		margin: 0;
+		font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
+			Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+		font-size: 16px;
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	*,
+	*::after,
+	*::before {
+		box-sizing: border-box;
+	}
+
+	/* Global class for elements that should only be accessible by screenreaders */
+	.sr-only {
+		position: absolute;
+		width: 1px;
+		height: 1px;
+		padding: 0;
+		margin: -1px;
+		overflow: hidden;
+		clip: rect(0, 0, 0, 0);
+		white-space: nowrap;
+		border: 0;
+	}
+`
+import { AppProps } from 'next/app'
 
 function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<GlobalStyle />
+			<Component {...pageProps} />
+		</>
+	)
 }
 
-export default App;
+export default App
