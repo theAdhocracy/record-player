212822a3cea457ffd57727667fc69f066539524c
theAdhocracy <murrayadcock@gmail.com>
2021-05-25 23:27:03 +0100

    :speech_balloon: Home: Better copy and visual hierarchy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
-import { Album, RecordAPI } from '../components/Album/Album'
+import { RecordAPI } from '../components/Album/Album'
 import Page from '../components/Page/Page'
+import { adjustColour } from '@utils/ColourAdjust'
+import { AlbumGrid } from '@components/AlbumGrid/AlbumGrid'
 
 const StyledPage = styled(Page)`
 	main {
-		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		grid-template-rows: min-content min-content 1fr;
-		gap: 1rem 6rem;
-		max-width: 100rem;
+		max-width: 110rem;
 		margin: 0 auto;
+		padding: 0 1rem;
 
 		h1,
 		p {
@@ -19,12 +18,48 @@ const StyledPage = styled(Page)`
 			text-align: center;
 		}
 
+		dfn,
+		span.dfn {
+			color: #354797;
+		}
+
 		h1 {
-			margin-bottom: 0;
+			margin-bottom: 3rem;
+			font-size: 5rem;
+			line-height: 0.85;
+
+			span {
+				display: block;
+				margin-bottom: 0.2em;
+				font-size: 0.35em;
+				text-transform: lowercase;
+				font-weight: 400;
+				font-style: italic;
+				color: ${adjustColour('#3f3f3f', 80)};
+			}
 		}
 
 		p {
 			margin: 1rem 0 3rem;
+
+			a {
+				font-weight: bold;
+				text-underline-position: auto;
+				text-decoration-line: underline;
+				text-decoration-color: #354797;
+				text-decoration-thickness: 3px;
+
+				:hover,
+				:focus {
+					color: #354797;
+					text-decoration-color: #000000;
+					outline: none;
+				}
+			}
+		}
+
+		@media screen and (min-width: 500px) {
+			padding: 0 5rem;
 		}
 	}
 `
@@ -41,14 +76,20 @@ export default function Home({ records }: HomeTypes): JSX.Element {
 				></link>
 			</Head>
 			<StyledPage>
-				<h1>Welcome to the Hall of Records</h1>
+				<h1>
+					<span>An ad hoc</span> Hall of Records
+				</h1>
 				<p>
-					You've stumbled into the personal record collection of{' '}
-					<a href="https://theadhocracy.co.uk/">@theAdhocracy</a> – feel free to take a look around!
+					<span className="dfn">
+						&#10100; <dfn title="discophilia">dis•co•phil•i•a</dfn> &#10101;
+					</span>{' '}
+					The enjoyment, collection, and study of musical recordings, often in the form of
+					phonograph records and/or CDs.
 				</p>
-				{records.map((record) => {
-					return <Album key={record.id} record={record} />
-				})}
+				<p>
+					Or, to put it another way: <a href="https://theadhocracy.co.uk/">my</a> music collection.
+				</p>
+				<AlbumGrid albums={records} />
 			</StyledPage>
 		</>
 	)
