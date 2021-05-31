f8d919115f7df1e47bb93197e1cc5e7634122e1e
theAdhocracy <murrayadcock@gmail.com>
2021-05-31 16:11:56 +0100

    :bug: Sluggify: Cater for artists with & in the name

--- utils/Sluggify.ts
+++ utils/Sluggify.ts
@@ -1,7 +1,8 @@
 export const Sluggify = (string: string) => {
 	const slug = string
 		.toLowerCase()
-		.replace(/\s/g, '-')
-		.replace(/[^a-zA-Z0-9-]*/g, '')
+		.replace(/\s/g, '-') // replace whitespace with -
+		.replace(/[^a-zA-Z0-9-]*/g, '') // remove all chars except alphanumeric and -
+		.replace('--', '-') // replace double -- with -; happens when illegal char is whole word e.g. &
 	return slug
 }
