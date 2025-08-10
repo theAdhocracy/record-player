8358a728f6f080057a3a2d729687e71eab11f92b
theAdhocracy <murrayadcock@gmail.com>
2025-08-10 16:15:11 +0100

    :fire: Remove Next.js

--- utils/Sluggify.ts
+++
@@ -1,8 +0,0 @@
-export const Sluggify = (string: string) => {
-	const slug = string
-		.toLowerCase()
-		.replace(/\s/g, '-') // replace whitespace with -
-		.replace(/[^a-zA-Z0-9-]*/g, '') // remove all chars except alphanumeric and -
-		.replace('--', '-') // replace double -- with -; happens when illegal char is whole word e.g. &
-	return slug
-}
