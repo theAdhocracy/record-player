ad1d8c03fa9738ef50cba83ea6e67d97efef3531
theAdhocracy <murrayadcock@gmail.com>
2025-08-25 16:58:12 +0100

    :bug: Handle collab albums

--- src/utils/Sluggify.ts
+++ src/utils/Sluggify.ts
@@ -2,6 +2,7 @@ export const Sluggify = (string: string) => {
 	const slug = string
 		.toLowerCase()
 		.replace(/\s/g, '-') // replace whitespace with -
+		.replace(/\".*?\"/g, '') // remove anything between quote marks (e.g. Damian "Jr. Gong" Marley => damian-marley)
 		.replace(/[^a-zA-Z0-9-]*/g, '') // remove all chars except alphanumeric and -
 		.replace('--', '-') // replace double -- with -; happens when illegal char is whole word e.g. &
 	return slug
